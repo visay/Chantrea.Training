96c84fc2dbe0cb51ecf2ef41d17552f2cef8012f
Visay Keo <keo@visay.info>
2013-02-02 22:26:41 +0700

    [TASK] Adjust to Flow 2.0

--- Classes/Chantrea/Training/Controller/StandardController.php
+++ Classes/Chantrea/Training/Controller/StandardController.php
@@ -2,7 +2,7 @@
 namespace Chantrea\Training\Controller;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "Chantrea.Training".          *
+ * This script belongs to the TYPO3 Flow package "Chantrea.Training".     *
  *                                                                        *
  *                                                                        */
 
@@ -16,82 +16,14 @@ use TYPO3\Flow\Annotations as Flow;
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Security\AccountRepository
-	 */
-	protected $accountRepository;
-	
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Security\AccountFactory
-	 */
-	protected $accountFactory;
-	
-	/**
-	 * @Flow\Inject
-	 * @var \TYPO3\Flow\Security\Authentication\AuthenticationManagerInterface
-	 */
-	protected $authenticationManager;
-	
-	/**
 	 * Index action
 	 *
 	 * @return void
 	 */
 	public function indexAction() {
-
-	}
-	
-	/**
-	 * Authenticate username and password
-	 * 
-	 * @return void
-	 */
-	public function authenticateAction() {
-		try {
-			$this->authenticationManager->authenticate();
-			$this->redirect('index', 'Training');
-		} catch (\TYPO3\Flow\Security\Exception\AuthenticationRequiredException $exception) {
-			$this->addFlashMessage('Wrong username or password.');
-			$this->redirect('index');
-		}
-	}
-	
-	/**
-	 * Logout action
-	 * 
-	 * @return void
-	 */
-	public function logoutAction() {
-		$this->authenticationManager->logout();
-		$this->addFlashMessage('Successfully logged out.');
-		$this->redirect('index');
-	}
-	
-	/**
-	 * Generate default account for login
-	 * 
-	 * @return void
-	 */
-	public function generateDefaultAccountAction() {
-		$this->accountRepository->removeAll();
-		
-		$identifier = 'visay';
-		$password = '12345';
-		$roles = array('Administrator');
-		$authenticationProviderName = 'DefaultProvider';
-		
-		$account = $this->accountFactory->createAccountWithPassword($identifier, $password, $roles, $authenticationProviderName);
-		$this->accountRepository->add($account);
-		$this->addFlashMessage('Account created! Username: "visay", Password: "12345"!');
-		$this->redirect('index');
-	}
-	
-	/**
-	 * Enter description here ...
-	 */
-	public function signupAction() {
-		
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
 	}
 
 }
---
+++ Classes/Chantrea/Training/Domain/Model/Topic.php
@@ -0,0 +1,71 @@
+<?php
+namespace Chantrea\Training\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Chantrea.Training".     *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A Topic
+ *
+ * @Flow\Entity
+ */
+class Topic {
+
+	/**
+	 * The title
+	 * @var string
+	 */
+	protected $title;
+
+	/**
+	 * The short description
+	 * @var string
+	 */
+	protected $shortDescription;
+
+
+	/**
+	 * Get the Topic's title
+	 *
+	 * @return string The Topic's title
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * Sets this Topic's title
+	 *
+	 * @param string $title The Topic's title
+	 * @return void
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * Get the Topic's short description
+	 *
+	 * @return string The Topic's short description
+	 */
+	public function getShortDescription() {
+		return $this->shortDescription;
+	}
+
+	/**
+	 * Sets this Topic's short description
+	 *
+	 * @param string $shortDescription The Topic's short description
+	 * @return void
+	 */
+	public function setShortDescription($shortDescription) {
+		$this->shortDescription = $shortDescription;
+	}
+
+}
+?>
