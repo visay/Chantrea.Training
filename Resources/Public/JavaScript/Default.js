a421dce042f0308143c6769a21d9f660ceaee6e1
Rayuth You <rayuth@web-essentials.asia>
2013-07-11 17:37:13 +0700

    [TASK] put datetimepicker

---
+++ Resources/Public/JavaScript/Default.js
@@ -0,0 +1,11 @@
+/*!
+ * Chantrea.Training v0.0.1
+ *
+ * Copyright 2013 Keo Visay
+ * Licensed under the LGPL-3.0+
+ */
+
+jQuery(document).ready(function(){
+	jQuery('#trainingDateFrom').datetimepicker();
+	jQuery('#trainingDateTo').datetimepicker();
+});
