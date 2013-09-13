4b351522c6d80447530e1ffc329fbef2d48ffaca
Chanthorn Ngeth <chanthorn.ngeth@gmail.com>
2013-09-13 17:49:37 +0700

    Add contact form

--- Classes/Chantrea/Training/Controller/StandardController.php
+++ Classes/Chantrea/Training/Controller/StandardController.php
@@ -179,6 +179,54 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	public function showAccountAction(Account $account) {
 		$this->view->assign('account', $account);
 	}
+
+	/**
+	 * Show contact form action
+	 *
+	 * @return Void
+	 */
+	public function showContactAction() {
+	}
+
+	/**
+	 * Send Contact action
+	 *
+	 * @param string  $firstName  The first name
+	 * @param string  $lastName   The last name
+	 * @param string  $email      The email address
+	 * @param string   $orgCompany   The organization / company
+	 * @param string   $message   The message
+	 *
+	 * @Flow\Validate(argumentName="firstName", type="NotEmpty")
+	 * @Flow\Validate(argumentName="lastName", type="NotEmpty")
+	 *
+	 * @Flow\Validate(argumentName="email", type="NotEmpty")
+	 * @Flow\Validate(argumentName="email", type="EmailAddress")
+	* @Flow\Validate(argumentName="message", type="NotEmpty")
+	 *
+	 * @return void
+	 */
+	public function sendContactAction($firstName, $lastName, $email, $orgCompany, $message) {
+		/*
+		echo($firstName);
+		echo($lastName);
+		echo($email);
+		echo($orgCompany);
+		echo($message);exit();
+
+		// TODO: read email from constant and render body from view
+		$mail = new \TYPO3\SwiftMailer\Message();
+		$mail->setFrom(array('noreply@visay.info' => 'Chantrea Training'))
+			->setTo(array($email => $user->getName()))
+			->setSubject('Account Creation')
+			->setFormat('html')
+			->setBody('Dear ' . $user->getName() . ',<br/><br/>Your account "' . $username .
+				'" has been created.<br/><br/>Best regards,<br/>Chantrea Training Team', 'text/html');
+		$mail->send();
+
+		$this->addFlashMessage('Your contact has been created successfully.');
+		$this->redirect('index');*/
+	}
 }
 
 ?>
